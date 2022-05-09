e55f10b5e8b473a8d085184e7dcf225d16799638
Red <18665356117@163.com>
2022-05-09 10:44:46 +0800

    1.增加语言

--- SwifterBaseKit.podspec
+++ SwifterBaseKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SwifterBaseKit'
-  s.version          = '0.3.2'
+  s.version          = '0.3.3'
   s.summary          = 'Project commonly used libraries.'
 
 # This description is used to generate tags and improve search results.
--- SwifterBaseKit/Until/Localize.swift
+++ SwifterBaseKit/Until/Localize.swift
@@ -9,9 +9,24 @@ import Foundation
 
 public enum LanguageType : String {
     case zh_Hans    = "zh-Hans"
+    case zh_Hant    = "zh-Hant"
     case en         = "en"
+    /// 日本语
     case ja         = "ja"
+    /// 韩语
     case ko         = "ko"
+    /// 法语
+    case fr         = "fr"
+    /// 意大利语
+    case it         = "it"
+    /// 西班牙语
+    case es         = "es"
+    /// 波兰语
+    case pl         = "pl"
+    /// 捷克
+    case cs         = "cs"
+    /// 德语
+    case de         = "de"
 }
 
 public struct Localize {
@@ -37,18 +52,20 @@ public struct Localize {
     public static func defaultLanguage() -> String {
         
         var defaultLanguage = LanguageType.en.rawValue
-        guard let temp = Locale.preferredLanguages.first else {
+        
+        guard let prefixLanguage = Locale.preferredLanguages.first else {
             return defaultLanguage
         }
-        guard var preferredLanguage = temp.components(separatedBy: "-").first else {
+        guard var preferredLanguage = prefixLanguage.components(separatedBy: "-").first else {
             return defaultLanguage
         }
-//        guard var preferredLanguage = Locale.current.identifier.components(separatedBy: "_").first else {
-//            return defaultLanguage
-//        }
-        /// 将zh-Hant zh变成本地的zh-Hans
-        if preferredLanguage == "zh-Hant" || preferredLanguage == "zh" {
-            preferredLanguage = "zh-Hans"
+        /// 这边处理简体和繁体
+        if preferredLanguage == "zh" {
+            if prefixLanguage.contains("zh-Hans") {
+                preferredLanguage = "zh-Hans"
+            }else {
+                preferredLanguage = "zh-Hant"
+            }
         }
         let availableLanguages = self.availableLanguages()
         if (availableLanguages.contains(preferredLanguage)) {
